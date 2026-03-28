41a5235adc689374ac29c7b51bd2ee60323044a6
HeitorSantAnna <thorsantanna2005@gmail.com>
2026-03-28 07:46:51 -0300

    Teste
    Testei algumas coisas, espero que isso não estrague o projeto

---
+++ Teste.gd
@@ -0,0 +1,13 @@
+extends Node
+
+Vector3 pos;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pos = new Vector3(10, 0, 0);
+	pass
