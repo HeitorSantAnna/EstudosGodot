35f42bc3cc95bd8f49367793136328cdbc53ab13
HeitorSantAnna <thorsantanna2005@gmail.com>
2026-03-31 17:13:57 -0300

    Teste
     Não deu carto

--- csg_sphere_3d.gd
+++ csg_sphere_3d.gd
@@ -11,10 +11,16 @@ var soma = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(soma + r * e)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += andar * 1 * delta
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		print("Olá mundo")
+	
+	if Input.is_key_pressed(KEY_D):
+		#position += andar * delta
+		print("Se movel para a direita")
 	pass
