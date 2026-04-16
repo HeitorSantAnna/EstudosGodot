84f55b885a0a6348b25b3edcc35c381375b81714
HeitorSantAnna <thorsantanna2005@gmail.com>
2026-04-16 18:51:09 -0300

    Estudos
    Fiz o player rotacionar a partir do código

---
+++ MoveSprite.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+var speed = 10
+var angular_speed = PI
+
+func _init() -> void:
+	print("Hello, World!!!")
+
+func _process(delta: float) -> void:
+	
+	#Aqui está fazendo o player rotacionar a partir do centro dele
+	rotation += angular_speed * delta
+	
+	#Aqui vai fazer o player rotacionar a partir de um centro, ele vai fazer um circulo
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity
