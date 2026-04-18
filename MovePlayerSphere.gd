dbb558a4ee433054829d58299e46248abc34a5ac
HeitorSantAnna <thorsantanna2005@gmail.com>
2026-04-18 16:07:08 -0300

    Teste de Projeto
    Vou tentar fazer o mesmo projeto que o Roque

---
+++ MovePlayerSphere.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+var dash = 2
+var accel = false
+var plane
+
+func _process(delta: float) -> void:
+	
+	var move := Input.get_vector("LeftMoviment", "RightMoviment", "UpMoviment", "DownMoviment")
+	var pos := Vector3(move.x, 0, move.y).normalized()
+	
+	if Input.is_action_pressed("Dash"):
+		accel = true
+	else:
+		accel = false
+	
+	if Input.is_action_just_pressed("Alterar Plano"):
+		plane = true
+	else: if Input.is_action_just_pressed("Alterar Plano") and plane:
+		plane = false
+	
+	if plane:
+		collision_layer = 2
+	else:
+		collision_layer = 1
+	
+	if accel:
+		dash = 4
+	else:
+		dash = 2
+	
+	position += pos * dash * delta
+	print(collision_layer)
--- MoveSprite.gd
+++ MoveSprite.gd
@@ -6,11 +6,17 @@ var angular_speed = PI
 func _init() -> void:
 	print("Hello, World!!!")
 
+
 func _process(delta: float) -> void:
 	
 	#Aqui está fazendo o player rotacionar a partir do centro dele
-	rotation += angular_speed * delta
-	
+	#rotation += angular_speed * delta
+
 	#Aqui vai fazer o player rotacionar a partir de um centro, ele vai fazer um circulo
-	var velocity = Vector2.UP.rotated(rotation) * speed
-	position += velocity
+	#var velocity = Vector2.UP.rotated(rotation) * speed
+	#position += velocity
+	
+	var moveup := Input.get_vector("LeftMoviment", "RightMoviment", "UpMoviment", "DownMoviment")
+	var direction := (Vector2(moveup.x, moveup.y).normalized())
+	
+	position += direction * 20
